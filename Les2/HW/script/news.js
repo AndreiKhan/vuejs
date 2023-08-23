13830e876c2307af62955bca315fb4e16e4679f6
«Andrei» <Spectacul9r@mail.ru>
2023-08-23 22:40:43 +0500

    les2

---
+++ Les2/HW/script/news.js
@@ -0,0 +1,43 @@
+new Vue({
+    el: '#news',
+    data: {
+        news: [
+            {
+                img: 'img/news1.png',
+                design: 'Kitchan Design',
+                title: 'Let’s Get Solution For Building Construction Work',
+                date: '26 December,2022',
+            },
+            {
+                img: 'img/news2.png',
+                design: 'Living Design',
+                title: 'Low Cost Latest Invented Interior Designing Ideas',
+                date: '22 December,2022',
+            },
+            {
+                img: 'img/news3.png',
+                design: 'Interior Design',
+                title: 'Best For Any Office & Business Interior Solution',
+                date: '25 December,2022',
+            },
+            {
+                img: 'img/news4.png',
+                design: 'Kitchan Design',
+                title: 'Let’s Get Solution For Building Construction Work',
+                date: '26 December,2022',
+            },
+            {
+                img: 'img/news5.png',
+                design: 'Living Design',
+                title: 'Low Cost Latest Invented Interior Designing Ideas',
+                date: '22 December,2022',
+            },
+            {
+                img: 'img/news6.png',
+                design: 'Interior Design',
+                title: 'Best For Any Office & Business Interior Solution',
+                date: '25 December,2022',
+            },
+        ],
+    }
+})
