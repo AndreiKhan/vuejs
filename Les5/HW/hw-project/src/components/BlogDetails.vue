3f464397fb843fd277712d9764fcb5b5be1f0312
«Andrei» <Spectacul9r@mail.ru>
2023-09-05 18:06:24 +0500

    les5

---
+++ Les5/HW/hw-project/src/components/BlogDetails.vue
@@ -0,0 +1,182 @@
+<template>
+    <div class="top">
+        <div class="background">
+            <div class="background_lines"></div>
+        </div>
+
+        <div class="title">
+        </div>
+
+        <BlogList></BlogList>
+    </div>
+</template>
+
+<script>
+import BlogList from './BlogList.vue'
+
+export default {
+    name: 'VueJsBlogList',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    components: {
+        BlogList,
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="sass">
+.title
+    background-image: url(../assets/banner_photo3.png)
+.blog
+    display: grid
+    gap: 52px
+    grid-template-columns: 2fr 1fr
+    padding-top: 200px
+    
+    &_details_description
+        &__title
+            color: #292F36
+            font-family: DM Serif Display
+            font-size: 50px
+            font-style: normal
+            font-weight: 400
+            line-height: 125%
+            letter-spacing: 1px
+            margin-bottom: 21px
+        &__titleImg
+            border-radius: 50px
+            margin-bottom: 46px
+        &_info
+            display: flex
+            justify-content: space-between
+            margin-bottom: 48px
+            &__date
+                color: #4D5053
+                font-family: Jost
+                font-size: 16px
+                font-style: normal
+                font-weight: 400
+                line-height: 150%
+                letter-spacing: 0.16px
+                text-transform: capitalize
+            &__bread
+                color: #4D5053
+                font-family: Jost
+                font-size: 16px
+                font-style: normal
+                font-weight: 400
+                line-height: 150%
+                letter-spacing: 0.16px
+                text-transform: capitalize
+        &__text
+            color: #4D5053
+            font-family: Jost
+            font-size: 22px
+            font-style: normal
+            font-weight: 400
+            line-height: 150%
+            letter-spacing: 0.22px
+            margin-bottom: 35px
+        &_quote
+            display: flex
+            flex-direction: column
+            align-items: center
+            justify-content: center
+            height: 267px
+            margin-bottom: 27px
+            border-radius: 50px
+            background: #F4F0EC
+            &__text
+                color: #CDA274
+                text-align: center
+                font-family: DM Serif Display
+                font-size: 25px
+                font-style: italic
+                font-weight: 400
+                line-height: 125%
+                letter-spacing: 0.5px
+                width: 351px
+        &__subtitle
+            color: #292F36
+            font-family: DM Serif Display
+            font-size: 50px
+            font-style: normal
+            font-weight: 400
+            line-height: 125%
+            letter-spacing: 1px
+            margin-bottom: 20px
+        &__list
+            &__item
+                display: flex
+                gap: 14px
+                list-style-type: none
+                color: #4D5053
+                font-family: Jost
+                font-size: 22px
+                font-style: normal
+                font-weight: 400
+                line-height: 150%
+                letter-spacing: 0.22px
+                margin-bottom: 32px
+                width: 735px
+                &__number
+                    list-style-position: inside
+                    list-style-type: decimal
+                    color: #CDA274
+                    font-family: DM Serif Display
+                    font-size: 20px
+                    font-style: normal
+                    font-weight: 400
+                    line-height: 125%
+                    letter-spacing: 0.4px
+        &__subtitleImg
+            border-radius: 50px
+            margin-bottom: 35px
+    &_tags
+        position: sticky
+        top: 50px
+        height: 200px
+        &__title
+            color: #292F36
+            font-family: DM Serif Display
+            font-size: 25px
+            font-style: normal
+            font-weight: 400
+            line-height: 125%
+            letter-spacing: 0.5px
+            padding-bottom: 24px
+        &_filters
+            display: flex
+            gap: 10px
+            flex-wrap: wrap
+            &__button
+                color: #292F36
+                text-align: center
+                font-family: Jost
+                font-size: 18px
+                font-style: normal
+                font-weight: 400
+                line-height: 125%
+                letter-spacing: 0.36px
+                padding: 9px 30px
+                border-radius: 10px
+                background: #F4F0EC
+                border: none
+                &:hover
+                    cursor: pointer
+.clicked
+    color: #FFF
+    background: #292F36
+.news_box
+    grid-template-columns: repeat(2, 1fr)
+    margin-bottom: 96px
+</style>
---
+++ Les5/HW/hw-project/src/components/NewsList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="news_box_element">
+            <img class="news_box_element__img" :src="require(`@/assets/${news.img}`)" alt="news_photo">
+            <a class="news_box_element__link"  href="#">{{ news.design }} Design</a>
+            
+            <h2 class="news_box_element__title">{{ news.title }}</h2>
+            
+            <div class="news_box_element_description">
+                <p class="news_box_element_description__text">{{ news.date }}</p>
+                
+
+                <a @click="detailes(news.id)" class="news_box_element_description__link" href="#"><svg width="52" height="53" viewBox="0 0 52 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="26" cy="26.267" r="26" fill="#F4F0EC"/>
+                    <path d="M23.7714 32.9527L29.7143 26.267L23.7714 19.5813" stroke="#292F36" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </a>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+    name: 'VueJsNewsList',
+    props: ['news'],
+
+    methods: {
+        detailes(blog) {
+            this.$emit('detailes', blog)
+        }
+    },
+};
+</script>
+
+<style lang="sass">
+
+</style>
